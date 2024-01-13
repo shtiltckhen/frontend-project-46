e783876cfe1d05c33e1e87f80615957923f9cd7a
Белина <shtiltckhen@gmail.com>
2024-01-13 22:40:07 +0500

    upgrade parser

--- src/parse.js
+++ src/parse.js
@@ -1,14 +1,6 @@
 import { load } from 'js-yaml';
 
 const parse = (data, ext) => {
-  // let result = {};
-  // if (ext === '.json') {
-  //  result = JSON.parse(data);
-  // } else if (ext === '.yaml' || ext === '.yml') {
-  //  result = load(data);
-  // }
-  // return result;
-
   switch (ext) {
     case '.json':
       return JSON.parse(data);
