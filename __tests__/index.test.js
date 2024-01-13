44360afdcd7ce4bc1fbdc44e0b1ca1022c3df8bc
Белина <shtiltckhen@gmail.com>
2024-01-13 22:33:12 +0500

    write tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,38 @@
+import parse from '../src/parse.js';
+
+import genDiff from '../src/index.js';
+
+const jsonAbsoluteFilepath1 = '/home/shtiltckhen/frontend-project-46/__fixtures__/file1.json';
+const jsonAbsoluteFilepath2 = '/home/shtiltckhen/frontend-project-46/__fixtures__/file2.json';
+
+const ymlAbsoluteFilepath1 = '/home/shtiltckhen/frontend-project-46/__fixtures__/file1.yml';
+const ymlAbsoluteFilepath2 = '/home/shtiltckhen/frontend-project-46/__fixtures__/file2.yml';
+
+const jsonFilepath1 = '__fixtures__/file1.json';
+const jsonFilepath2 = '__fixtures__/file2.json';
+
+const ymlFilepath1 = '__fixtures__/file1.yml';
+const ymlFilepath2 = '__fixtures__/file2.yml';
+
+const expectedOutput = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+test('parse', () => {
+  expect(() => {
+    parse('');
+  }).toThrow();
+});
+
+test('genDiff', () => {
+  expect(genDiff(jsonFilepath1, jsonFilepath2)).toBe(expectedOutput);
+  expect(genDiff(jsonAbsoluteFilepath1, jsonAbsoluteFilepath2)).toEqual(expectedOutput);
+  expect(genDiff(ymlFilepath1, ymlFilepath2)).toBe(expectedOutput);
+  expect(genDiff(ymlAbsoluteFilepath1, ymlAbsoluteFilepath2)).toBe(expectedOutput);
+  expect(genDiff(jsonFilepath1, ymlFilepath2)).toBe(expectedOutput);
+});
