b5b87926e4cbfbc4700efeada3275a5f0dad3c24
Белина <shtiltckhen@gmail.com>
2024-01-16 02:55:03 +0500

    update makeTree

--- src/makeTree.js
+++ src/makeTree.js
@@ -20,34 +20,22 @@ const makeTree = (file1, file2) => {
     if (keys2.includes(key)) {
       const value2 = file2[key];
       if (isObject(value1) && isObject(value2)) {
-        return { status: 'haveChildren', children: true, key, value: makeTree(value1, value2) };
+        return { status: 'haveChildren', key, value: makeTree(value1, value2) };
       }
       if (value1 === value2) {
         return { status: 'unchanged', key, value: value1 };
       }
-      if (isObject(value1)) {
-        return { status: 'changed', children: true, key, oldValue: getValue(value1), newValue: value2 };
-      }
-      if (isObject(value2)) {
-        return { status: 'changed', children: true, key, oldValue: value1, newValue: getValue(value2) };
-      }
-      if (value1 !== value2) {
-        return { status: 'changed', children: true, key, oldValue: value1, newValue: value2 };
-      }
-    } else if (isObject(value1)) {
-      return { status: 'removed', children: true, key, value: getValue(value1) };
+      return {
+        status: 'changed', key, oldValue: getValue(value1), newValue: getValue(value2),
+      };
     }
-    return { status: 'removed', key, value: value1 };
+    return { status: 'removed', key, value: getValue(value1) };
   });
   const restKeys = keys2
     .filter((key) => !keys1.includes(key))
-    .map((key) => {
-      if (isObject(file2[key])) {
-        return { status: 'added', children: true, key, value: getValue(file2[key]) };
-      }
-      return { status: 'added', key, value: file2[key] };
-    });
+    .map((key) => ({ status: 'added', key, value: getValue(file2[key]) }));
   tree.push(restKeys);
+
   return tree.flat().sort((a, b) => {
     const x = a.key >= b.key ? 1 : -1;
     return x;
