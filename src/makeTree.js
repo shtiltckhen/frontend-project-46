750baef1dfc5d38ae4bd80322a333d48dd74ae23
Белина <shtiltckhen@gmail.com>
2024-01-16 02:28:59 +0500

    update makeTree

--- src/makeTree.js
+++ src/makeTree.js
@@ -1,49 +1,52 @@
 const isObject = (node) => Object.prototype.toString.call(node) === '[object Object]';
 
-const getValue = (value) => {
-  if (!isObject(value)) {
-    return value;
+const getValue = (node) => {
+  if (!isObject(node)) {
+    return node;
   }
-  const keys = Object.keys(value);
+  const keys = Object.keys(node);
   return keys.map((key) => {
-    const newValue = isObject(value[key]) ? getValue(value[key]) : value[key];
-    return { status: 'unchanged', key, value: newValue };
+    const value = node[key];
+    const newValue = isObject(value) ? getValue(value) : value;
+    return { key, value: newValue };
   });
 };
 
 const makeTree = (file1, file2) => {
   const keys1 = Object.keys(file1);
   const keys2 = Object.keys(file2);
-  const tree = keys1.reduce((acc, key) => {
+  const tree = keys1.map((key) => {
     const value1 = file1[key];
-    const value2 = file2[key];
     if (keys2.includes(key)) {
+      const value2 = file2[key];
       if (isObject(value1) && isObject(value2)) {
-        acc.push({ status: 'unchanged', key, value: makeTree(value1, value2) });
-      } else if (value1 === value2) {
-        acc.push({ status: 'unchanged', key, value: value1 });
-      } else {
-        acc.push(
-          {
-            status: 'changed', action: 'removed', key, value: getValue(value1),
-          },
-          {
-            status: 'changed', action: 'added', key, value: getValue(value2),
-          },
-        );
+        return { status: 'haveChildren', children: true, key, value: makeTree(value1, value2) };
       }
-    } else {
-      acc.push({
-        status: 'changed', action: 'removed', key, value: getValue(value1),
-      });
+      if (value1 === value2) {
+        return { status: 'unchanged', key, value: value1 };
+      }
+      if (isObject(value1)) {
+        return { status: 'changed', children: true, key, oldValue: getValue(value1), newValue: value2 };
+      }
+      if (isObject(value2)) {
+        return { status: 'changed', children: true, key, oldValue: value1, newValue: getValue(value2) };
+      }
+      if (value1 !== value2) {
+        return { status: 'changed', children: true, key, oldValue: value1, newValue: value2 };
+      }
+    } else if (isObject(value1)) {
+      return { status: 'removed', children: true, key, value: getValue(value1) };
     }
-    return acc;
-  }, []);
+    return { status: 'removed', key, value: value1 };
+  });
   const restKeys = keys2
     .filter((key) => !keys1.includes(key))
-    .map((key) => ({
-      status: 'changed', action: 'added', key, value: getValue(file2[key]),
-    }));
+    .map((key) => {
+      if (isObject(file2[key])) {
+        return { status: 'added', children: true, key, value: getValue(file2[key]) };
+      }
+      return { status: 'added', key, value: file2[key] };
+    });
   tree.push(restKeys);
   return tree.flat().sort((a, b) => {
     const x = a.key >= b.key ? 1 : -1;
