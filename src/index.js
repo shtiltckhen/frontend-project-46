b67d45d814e101841154af7c40bf8aa807caf315
Белина <shtiltckhen@gmail.com>
2024-01-15 10:45:46 +0500

    update formaterStylish

--- src/index.js
+++ src/index.js
@@ -2,46 +2,30 @@ import { readFileSync } from 'node:fs';
 import { cwd } from 'node:process';
 import { resolve, extname } from 'node:path';
 import parse from './parse.js';
+import makeTree from './makeTree.js';
 
-const getDiffFiles = (file1, file2) => {
-  const keys1 = Object.keys(file1);
-  const keys2 = Object.keys(file2);
-  const result = keys1.reduce((acc, key) => {
-    if (keys2.includes(key) && file1[key] === file2[key]) {
-      acc.push({ key, value: file1[key], status: 'unchanged' });
-    } else if (keys2.includes(key) && file1[key] !== file2[key]) {
-      acc.push({ key, value: file1[key], status: 'removed' });
-      acc.push({ key, value: file2[key], status: 'added' });
-    } else if (!keys2.includes(key)) {
-      acc.push({ key, value: file1[key], status: 'removed' });
-    }
-    return acc;
-  }, []);
-  keys2.filter((key) => !keys1.includes(key))
-    .map((key) => result.push({ key, value: file2[key], status: 'added' }));
-  return result.sort((a, b) => {
-    const x = a.key >= b.key ? 1 : -1;
-    return x;
-  });
-};
+const fotmaterStylish = (tree, depth = 1) => {
+  const getValue = (value) => {
+    const newValue = Array.isArray(value)
+      ? fotmaterStylish(value, depth + 1)
+      : value;
+    return newValue;
+  };
 
-const outputDiff = (diff) => {
-  const result = diff.reduce((acc, string) => {
-    switch (string.status) {
+  const replacer = '    ';
+
+  const result = tree.map((line) => {
+    switch (line.status) {
       case 'unchanged':
-        acc.push(`    ${string.key}: ${string.value}`);
-        break;
-      case 'removed': acc.push(`  - ${string.key}: ${string.value}`);
-        break;
-      case 'added': acc.push(`  + ${string.key}: ${string.value}`);
-        break;
+        return `${replacer.repeat(depth)}${line.key}: ${getValue(line.value)}`;
+      case 'changed':
+        return `${replacer.repeat(depth - 1)}${line.action === 'removed' ? '  - ' : '  + '}${line.key}: ${getValue(line.value)}`;
       default:
-        throw new Error(`Unexpected status: ${string.status}`);
+        throw new Error(`Unexpected status: ${line.status}`);
     }
-    return acc;
-  }, []);
+  });
   result.unshift('{');
-  result.push('}');
+  result.push(`${replacer.repeat(depth - 1)}}`);
   return result.join('\n');
 };
 
@@ -58,6 +42,6 @@ export default (filepath1, filepath2) => {
 
   const file1 = parse(data1, ext1);
   const file2 = parse(data2, ext2);
-  const diff = getDiffFiles(file1, file2);
-  return outputDiff(diff);
+  const diff = makeTree(file1, file2);
+  return fotmaterStylish(diff);
 };
