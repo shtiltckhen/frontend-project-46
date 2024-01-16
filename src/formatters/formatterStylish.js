b4ccb02c5fa1995868cb47d97460917e3e007753
Белина <shtiltckhen@gmail.com>
2024-01-16 13:06:35 +0500

    update formatter stylish

--- src/formatters/formatterStylish.js
+++ src/formatters/formatterStylish.js
@@ -1,32 +1,29 @@
-const getValue = (curValue, replacer, replacerCount, level) => {
-  if (!Array.isArray(curValue)) {
-    return curValue;
-  }
-  const result = curValue.map((node) => `${replacer.repeat(replacerCount * level)}${node.key}: ${getValue(node.value, replacer, replacerCount, level + 1)}`);
-
-  result.unshift('{');
-  result.push(`${replacer.repeat(replacerCount * (level - 1))}}`);
-  return result.join('\n');
-};
-
 const formatterStylish = (tree, replacer = ' ', replacerCount = 4, depth = 1) => {
-  const getString = (key, value, sign) => `${replacer.repeat(replacerCount * (depth - 1))}  ${sign} ${key}: ${getValue(value, replacer, replacerCount, depth + 1)}`;
-
+  const getString = (key, value, sign, level) => `${replacer.repeat(replacerCount * (level - 1))}  ${sign} ${key}: ${value}`;
+  const getValue = (curValue, level) => {
+    if (!Array.isArray(curValue)) {
+      return curValue;
+    }
+    const result = curValue.map((node) => getString(node.key, getValue(node.value, level + 1), ' ', level));
+    result.unshift('{');
+    result.push(`${replacer.repeat(replacerCount * (level - 1))}}`);
+    return result.join('\n');
+  };
   const formatedTree = tree.reduce((acc, node) => {
     switch (node.status) {
       case 'haveChildren':
-        acc.push(`${replacer.repeat(replacerCount * depth)}${node.key}: ${formatterStylish(node.value, replacer, replacerCount, depth + 1)}`);
+        acc.push(getString(node.key, formatterStylish(node.value, replacer, replacerCount, depth + 1), ' ', depth));
         break;
       case 'unchanged':
-        acc.push(`${replacer.repeat(replacerCount * depth)}${node.key}: ${node.value}`);
+        acc.push(getString(node.key, node.value, ' ', depth));
         break;
       case 'changed':
-        acc.push(getString(node.key, node.oldValue, '-'));
-        acc.push(getString(node.key, node.newValue, '+'));
+        acc.push(getString(node.key, getValue(node.oldValue, depth + 1), '-', depth));
+        acc.push(getString(node.key, getValue(node.newValue, depth + 1), '+', depth));
         break;
       case 'added':
       case 'removed':
-        acc.push(getString(node.key, node.value, node.status === 'added' ? '+' : '-'));
+        acc.push(getString(node.key, getValue(node.value, depth + 1), node.status === 'added' ? '+' : '-', depth));
         break;
       default:
         throw new Error(`Unexpected status: ${node.status}`);
