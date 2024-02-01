bd006d11be02fcd14bcd56662c4f7a983b5fc17e
Белина <shtiltckhen@gmail.com>
2024-02-01 15:21:40 +0500

    update plain

--- src/formatters/formatterPlain.js
+++ src/formatters/formatterPlain.js
@@ -10,24 +10,23 @@ const getValue = (value) => {
 
 const formatterPlain = (tree, parent = '') => tree
   .map((node) => {
-    let string;
     const nameProperty = parent + node.key;
     if (node.status === 'hasChildren') {
-      string = formatterPlain(node.children, `${nameProperty}.`);
+      return formatterPlain(node.children, `${nameProperty}.`);
     }
     if (node.status === 'changed') {
       const oldValue = Object.hasOwn(node, 'oldValue') ? getValue(node.oldValue) : getValue(node.children);
       const newValue = Object.hasOwn(node, 'newValue') ? getValue(node.newValue) : getValue(node.children);
-      string = `Property '${nameProperty}' was updated. From ${oldValue} to ${newValue}`;
+      return `Property '${nameProperty}' was updated. From ${oldValue} to ${newValue}`;
     }
     if (node.status === 'removed') {
-      string = `Property '${nameProperty}' was removed`;
+      return `Property '${nameProperty}' was removed`;
     }
     if (node.status === 'added') {
       const value = Object.hasOwn(node, 'value') ? getValue(node.value) : getValue(node.children);
-      string = `Property '${nameProperty}' was added with value: ${value}`;
+      return `Property '${nameProperty}' was added with value: ${value}`;
     }
-    return string;
+    return '';
   })
   .filter(Boolean)
   .join('\n');
