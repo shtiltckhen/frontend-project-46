a7771bd968772d052d7f9efad7c7849f55b3ed2e
Белина <shtiltckhen@gmail.com>
2024-02-02 19:48:00 +0500

    update stylish

--- src/formatters/formatterStylish.js
+++ src/formatters/formatterStylish.js
@@ -4,28 +4,33 @@ const formatterStylish = (tree, replacer = ' ', replacerCount = 4, depth = 1) =>
   const getIndent = (level) => (replacer.repeat(replacerCount * (level - 1)));
   const formatedTree = tree.flatMap((node) => {
     const indent = getIndent(depth);
-    if (node.status === 'hasChildren') {
-      return getString(node.key, formatterStylish(node.children, replacer, replacerCount, depth + 1), ' ', indent);
+    switch (node.status) {
+      case 'hasChildren':
+        return getString(node.key, formatterStylish(node.children, replacer, replacerCount, depth + 1), ' ', indent);
+      case 'unchanged':
+        return getString(node.key, node.value, ' ', indent);
+      case 'changed': {
+        const oldValue = Object.hasOwn(node, 'oldValue') ? node.oldValue : formatterStylish(node.children, replacer, replacerCount, depth + 1);
+        const newValue = Object.hasOwn(node, 'newValue') ? node.newValue : formatterStylish(node.children, replacer, replacerCount, depth + 1);
+        return [getString(node.key, oldValue, '-', indent),
+          getString(node.key, newValue, '+', indent)];
+      }
+      case 'added':
+      case 'removed': {
+        const value = node.children.length === 0
+          ? node.value
+          : formatterStylish(node.children, replacer, replacerCount, depth + 1);
+        return getString(node.key, value, node.status === 'added' ? '+' : '-', indent);
+      }
+      case undefined: {
+        const value = node.children.length === 0
+          ? node.value
+          : formatterStylish(node.children, replacer, replacerCount, depth + 1);
+        return getString(node.key, value, ' ', indent);
+      }
+      default:
+        throw new Error(`Unexpected status: ${node.status}`);
     }
-    if (node.status === 'unchanged') {
-      return getString(node.key, node.value, ' ', indent);
-    }
-    if (node.status === 'changed') {
-      const oldValue = Object.hasOwn(node, 'oldValue') ? node.oldValue : formatterStylish(node.children, replacer, replacerCount, depth + 1);
-      const newValue = Object.hasOwn(node, 'newValue') ? node.newValue : formatterStylish(node.children, replacer, replacerCount, depth + 1);
-      return [getString(node.key, oldValue, '-', indent),
-        getString(node.key, newValue, '+', indent)];
-    }
-    if (node.status === 'added' || node.status === 'removed') {
-      const value = node.children.length === 0
-        ? node.value
-        : formatterStylish(node.children, replacer, replacerCount, depth + 1);
-      return getString(node.key, value, node.status === 'added' ? '+' : '-', indent);
-    }
-    const value = node.children.length === 0
-      ? node.value
-      : formatterStylish(node.children, replacer, replacerCount, depth + 1);
-    return getString(node.key, value, ' ', indent);
   });
   const result = ['{', ...formatedTree, `${getIndent(depth)}}`];
   return result.join('\n');
