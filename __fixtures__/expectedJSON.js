40b22780414875b72b4cea5c5028270a85d62af6
Белина <shtiltckhen@gmail.com>
2024-02-02 19:47:31 +0500

    remove test files to __fixture__

---
+++ __fixtures__/expectedJSON.js
@@ -0,0 +1,79 @@
+export default `{
+ "key": "common",
+ "status": "hasChildren",
+ "children": [
+  {
+   "key": "follow",
+   "status": "added",
+   "value": false,
+   "children": []
+  },
+  {
+   "key": "setting1",
+   "status": "unchanged",
+   "value": "Value 1",
+   "children": []
+  },
+  {
+   "key": "setting2",
+   "status": "removed",
+   "value": 200,
+   "children": []
+  },
+  {
+   "key": "setting3",
+   "status": "changed",
+   "oldValue": true,
+   "newValue": null,
+   "children": []
+  },
+  {
+   "key": "setting4",
+   "status": "added",
+   "value": "blah blah",
+   "children": []
+  },
+  {
+   "key": "setting5",
+   "status": "added",
+   "children": [
+    {
+     "key": "key5",
+     "value": "value5",
+     "children": []
+    }
+   ]
+  },
+  {
+   "key": "setting6",
+   "status": "hasChildren",
+   "children": [
+    {
+     "key": "doge",
+     "status": "hasChildren",
+     "children": [
+      {
+       "key": "wow",
+       "status": "changed",
+       "oldValue": "",
+       "newValue": "so much",
+       "children": []
+      }
+     ]
+    },
+    {
+     "key": "key",
+     "status": "unchanged",
+     "value": "value",
+     "children": []
+    },
+    {
+     "key": "ops",
+     "status": "added",
+     "value": "vops",
+     "children": []
+    }
+   ]
+  }
+ ]
+}`;
